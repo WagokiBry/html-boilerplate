4bc9419dff4d4bbc777b975358b078c4a46e15f4
Bryan <bryanwagoki2016@gmail.com>
2025-12-18 11:52:54 +0300

    Write a function constructor to grab values from a form and store them in an array

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 let log = console.log;
+
 let container = document.querySelector(".container");
-log(container)
+let button = document.querySelector("button");
+let title = document.querySelector("#title");
+let pages = document.querySelector("#pages");
+
+let arr = [];
+
+function Book (tit, page) {
+     this.title = tit;
+      this.pages = page;
+  }
+
+
+button.addEventListener("click",(e)=>{
+  // e.preventDefault();
+let harry = new Book (`${title.value}`, `${pages.value}`)
+arr.push(harry);
+title.value = "";
+pages.value = "";
+})
+log(arr)

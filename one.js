0f082a495d6cb1ed3710a697b51a864a91dd1bb1
Bryan <bryanwagoki2016@gmail.com>
2025-12-21 13:19:19 +0300

    Add export and import syntax for es6 modules

--- one.js
+++ one.js
@@ -1,5 +1,10 @@
-const greeting = (() => {
-  const greetingString = "Hello, Odinite!";
-  const farewellString = "Bye bye, Odinite!";
-  return greetingString;
-})();
+// const greeting = (() => {
+//   const greetingString = "Hello, Odinite! I am Bryan";
+//   const farewellString = "Bye bye, Odinite!";
+//   return greetingString;
+// })();
+
+export const greeting = "Hello, Odinite!";
+export const farewell = "Bye bye, Odinite!";
+
+export default "Brayyooo una default "
